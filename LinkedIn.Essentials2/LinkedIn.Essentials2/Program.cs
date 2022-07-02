5e24b403d63ff873c27999ca42a64d974bfa763a
Gayashan Wagachchi <gayashan4lk@gmail.com>
2022-07-02 12:14:48 +0530

    understanding generics

--- LinkedIn.Essentials2/LinkedIn.Essentials2/Program.cs
+++ LinkedIn.Essentials2/LinkedIn.Essentials2/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Essentials2.Generics;
+
+var p1 = new Person
+{
+    FirstName = "Matt",
+    LastName = "Damon",
+    Age = 45,
+};
+
+var p2 = new Person
+{
+    FirstName = "Robert",
+    LastName = "Downey",
+    Age = 25
+};
+
+Swap<Person>(ref p1, ref p2);
+
+Console.WriteLine($"Person 1: {p1.FirstName} {p1.LastName} and Person 2: {p2.FirstName} {p2.LastName}");
+
+int x = 3, y = 8;
+
+Swap<int>(ref x, ref y);
+
+Console.WriteLine($"x: {x} and y: {y}");
+
+static void Swap<T>(ref T first, ref T second)
+{
+    T temp = first;
+    first = second;
+    second = temp;
+}
