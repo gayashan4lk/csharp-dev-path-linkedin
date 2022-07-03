6af01ac1de4c4d8d729d694c5d6f26d655fd721b
Gayashan Wagachchi <gayashan4lk@gmail.com>
2022-07-03 18:19:10 +0530

    other uses of lambda

--- LinkedIn.Essentials2/Essentials2.Generics/Employee.cs
+++ LinkedIn.Essentials2/Essentials2.Generics/Employee.cs
@@ -8,9 +8,18 @@ namespace Essentials2.Generics
 {
     public class Employee
     {
+        public Employee(string Fname) => FirstName = Fname;
+
+        private string f_name;
         public int Id { get; set; }
-        public string FirstName { get; set; }
+        public string FirstName
+        {
+            get => f_name;
+            init => f_name = value;
+        }
         public string LastName { get; set; }
         public DateOnly? JoinedDate { get; set; }
+
+        public override string ToString() => $"{FirstName}_{LastName}";
     }
 }
